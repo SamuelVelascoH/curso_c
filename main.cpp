a11eb744bab6a9532bc6e284ea948a9d07c1122f
Samuel Velasco <samuelvelazco184@gmail.com>
2023-11-20 17:30:22 -0600

    triangulo

--- main.cpp
+++ main.cpp
@@ -5,30 +5,19 @@
 
 using namespace std;
 int main () {
-    int n;
-    int pos=1;
+int n=5,j,i;
 
-    cin>>n;
-    string es = "";
-    int size = n*n;
+for(i=1; i<=n; i++){
+    for(j=1; j<=n-i; j++)
+        cout<<" ";
+    for(j=1; j<= 2*i-1; j++)
+        cout<<"*";
+    cout<<endl;
+}
 
-    while (pos<=size){
-
-
-        if(pos % n != 0 & (pos-1) % n != 0 & pos > n & pos < (size-n)){
-            es += " ";
-        }else{
-            es += "*";
-        }
-        if(pos%n == 0){
-            es+="\n";
-        }
-        pos++;
-
-    }
-    cout<<es;
 
 }
+
 
 
 
