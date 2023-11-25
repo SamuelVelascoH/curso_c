31dfac8c6f7ae85b8577ad1379ff8afb2a00f63e
Samuel Velasco <samuelvelazco184@gmail.com>
2023-11-24 20:23:12 -0600

    Solucion Chat GPT

--- main.cpp
+++ main.cpp
@@ -2,22 +2,46 @@
 #include <windows.h>
 #include <conio.h>
 #include <math.h>
-
+#include <stdio.h>
 using namespace std;
 int main () {
-    int n=8;
-    //cout<<"ingresa un numero";
-    //cin>>n;
-    for (int i=1,x=2; i<=n; ++i, x++) {
-       if(x%2!=0){
-           cout<<"0";
-       }
-       else cout<<"1";
-        if(x>2){
-            x=0;
+    int n;
+
+    // Solicitar al usuario que ingrese un número
+    printf("Ingrese un número N: ");
+    scanf("%d", &n);
+
+    // Verificar si el número es válido
+    if (n <= 0) {
+        printf("Por favor, ingrese un número positivo.\n");
+        return 1; // Salir del programa con un código de error
+    }
+
+    // Inicializar una variable para alternar entre 1 y 2 ceros
+    int ceros = 1;
+
+    // Generar el patrón solicitado
+    for (int i = 1; i <= n; i++) {
+        // Imprimir '1' si ceros es impar, '00' si es par
+        if (ceros % 2 == 1) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+
+        // Alternar entre 1 y 2 ceros
+        ceros++;
+
+        // Si ceros llega a 3, reiniciar en 1
+        if (ceros > 2) {
+            ceros = 1;
         }
     }
 
+    // Salto de línea al final
+    printf("\n");
+
+    return 0; // Indicar que el programa se ejecutó con éxito
 }
 
 
