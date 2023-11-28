cabc957bb157183d7510fd7594b481af21775cc3
Samuel Velasco <samuelvelazco184@gmail.com>
2023-11-27 20:59:59 -0600

    Imprime_el_total_del_arreglo_y_el_contenido_al_reves

--- main.cpp
+++ main.cpp
@@ -5,15 +5,82 @@
 
 using namespace std;
 int main () {
-    int n=8;
-    // 100010001000100010
-    //cin>>n;
+    int n, total=0;
+    int tam[n];
+    cin>>n;
 
     for (int i = 0; i < n; ++i) {
-        if(i%3==0 ){
-            cout<<"1";
+       cin>>tam[i];// 1 2 3 4 5
+
+    }
+    for (int i = n-1; i>=0; --i){
+            cout<<tam[i]<<" ";
+
         }
-        else cout<<"0";
+    for (int i = 0; i < n  ; ++i) {
+        total=total+tam[i];
+    }
+cout<<endl<<total;
+
+
     }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    int tamFilas = 4;
+    int tamCols = 7;
+    string horarios[tamFilas][tamCols];
+
+    */
+/*
+     [ hora ][   domingo  ][ lunes ][ martes ][ miercoles ][ jueves ][ viernes ]
+     [ 7-8  ][ programing ][ artes ][       ][            ][        ][         ]
+     [      ][            ][       ][       ][            ][        ][         ]
+     [      ][            ][       ][       ][            ][        ][         ]
+     [      ][            ][       ][       ][            ][        ][         ]
+     *//*
+
+    horarios[0][0] = "Hora";
+    horarios[0][1] = "Materia";
+    horarios[0][2] = "Lunes";
+    horarios[0][3] = "Martes";
+    horarios[0][4] = "Miercoles";
+    horarios[0][5] = "Jueves";
+    horarios[0][6] = "Viernes";
+
+    horarios[1][0] = "7 - 8";
+    horarios[1][1] = "Programing";
+    horarios[1][2] = "artes";
+    cout<<endl;
+    for (int i = 0; i < tamFilas; ++i) {
+        for (int j = 0; j < tamCols; ++j) {
+            cout<<"["<<horarios[i][j]<<"\t"<<"]";
+        }
+        cout<<endl;
+    }
+*/
+
+
+
+
+
